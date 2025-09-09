46752d85d7c72ad59686c43153c33140d1281d1d
Reginah24 <r.majura@alustudent.com>
2025-09-09 17:39:25 +0200

    base file created

---
+++ models/base_model.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Defines the BaseModel class."""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+	"""Base class for all models."""
+
+	def __init__(self, *args, **kwargs):
+		if kwargs:
+			for key, value in kwargs.items():
+				if key == "created_at" or key == "updated_at":
+					setattr(self, key, datetime.fromisoformat(value))
+				elif key != "__class__":
+					setattr(self, key, value)
+		else:
+			self.id = str(uuid.uuid4())
+			self.created_at = datetime.now()
+			self.updated_at = datetime.now()
+
+	def __str__(self):
+		return "[{}] ({}) {}".format(
+			self.__class__.__name__, self.id, self.__dict__)
+
+	def save(self):
+		self.updated_at = datetime.now()
+
+	def to_dict(self):
+		d = self.__dict__.copy()
+		d["__class__"] = self.__class__.__name__
+		d["created_at"] = self.created_at.isoformat()
+		d["updated_at"] = self.updated_at.isoformat()
+		return d
