3bb767430248c652948c6b811989c6c9ae71ab08
Reginah24 <r.majura@alustudent.com>
2025-09-14 19:13:53 +0200

    base_model file

---
+++ models/base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""BaseModel module for AirBnB clone project."""
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """Defines all common attributes/methods for other classes."""
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.fromisoformat(value))
+                elif key != "__class__":
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Updates the public instance attribute updated_at with current datetime."""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance."""
+        d = self.__dict__.copy()
+        d["__class__"] = self.__class__.__name__
+        d["created_at"] = self.created_at.isoformat()
+        d["updated_at"] = self.updated_at.isoformat()
+        return d
